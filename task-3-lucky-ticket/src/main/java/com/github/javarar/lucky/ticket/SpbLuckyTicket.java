57dc42884c170061debb308bafb77bca5ad81328
Tikhonov <aleksandr.a.tikhonov@rt.ru>
2024-07-31 09:57:16 +0700

    answer

--- task-3-lucky-ticket/src/main/java/com/github/javarar/lucky/ticket/SpbLuckyTicket.java
+++ task-3-lucky-ticket/src/main/java/com/github/javarar/lucky/ticket/SpbLuckyTicket.java
@@ -1,6 +1,5 @@
 package com.github.javarar.lucky.ticket;
 
-import java.util.List;
 import java.util.stream.Collectors;
 import java.util.stream.Stream;
 
@@ -10,11 +9,12 @@ public class SpbLuckyTicket extends LuckyTicket {
         var array = number.split("");
         var result = Stream.of(array)
                 .map(Integer::parseInt)
-                .collect(Collectors.partitioningBy(s -> s % 2 == 0));
-        return sum(result.get(true)) == sum(result.get(false));
+                .collect(
+                        Collectors.partitioningBy(s -> s % 2 == 0,
+                        Collectors.summingInt(s -> s)
+                        )
+                );
+        return result.get(true).equals(result.get(false));
     }
 
-    private int sum(List<Integer> list) {
-        return list.stream().mapToInt(Integer::intValue).sum();
-    }
 }
